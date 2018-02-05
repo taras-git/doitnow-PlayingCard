ba6b26ede0824e0ac04fe85043551c1d01406293
Swift iOS <swiftios@4nloshtykhno.ad.here.com>
2018-02-05 14:11:23 +0200

    new commit

--- PlayingCard/ViewController.swift
+++ PlayingCard/ViewController.swift
@@ -10,14 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var deck = PlayingCardDeck()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
+        for _ in 1...10 {
+            if let card = deck.draw(){
+                print("\(card)")
+            }
+        }
     }
 
 
